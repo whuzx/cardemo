68ba5cbcd503bac4278ccd0b7187ef2fd8254c36
BIgpanda <38294061@qq.com>
2025-02-07 14:07:18 +0800

    Update sidebar.tsx
    智能化跃迁

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -250,8 +250,8 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="DeepSeek"
+        subTitle="助力东风智能化跃迁."
         logo={<ChatGptIcon />}
         shouldNarrow={shouldNarrow}
       >
